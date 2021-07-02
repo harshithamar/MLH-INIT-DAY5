b59fd2022a33f875e279e1199559cf89bd261fd3
Binish077 <bsh.pat77@gmail.com>
2021-07-02 18:47:15 +0530

    Create pclass.py

---
+++ pclass.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 18:44:11 2021
+
+@author: bshpa
+"""
+
+#class for passwords
+
+class Passwords:
+    def __init__(self, platform, email, username, password):
+        self.platform=platform
+        self.email=email
+        self.username=username
+        self.password=password
